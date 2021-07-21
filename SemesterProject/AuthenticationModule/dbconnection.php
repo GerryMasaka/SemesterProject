d386f8b8137022310d2e2009302a47ddf92b0dd8
GerryMasaka <77191877+GerryMasaka@users.noreply.github.com>
2021-07-21 13:41:11 +0300

    Add files via upload

---
+++ SemesterProject/AuthenticationModule/dbconnection.php
@@ -0,0 +1,17 @@
+<?php
+$dbserver="localhost";
+$dbuser="root";
+$password="";
+$dbname="wefix";
+
+$conn=mysqli_connect($dbserver,$dbuser,$password,$dbname);
+
+if($conn){
+    //echo "Connected Successfully \r\n";
+    
+}
+else{
+    echo "\r\nDid not connect" .mysqli_connect_error();
+}
+
+?>
